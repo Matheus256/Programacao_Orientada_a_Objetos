4e90065f0f89ab484773bf73827c06b7c987b0e8
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2024-12-16 17:21:28 -0300

    Create ChapeuSeletor.h

---
+++ Atividade_4/Questao9/ChapeuSeletor.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+//Para gerar numeros aleatórios
+#include <cstdlib>
+//#include <ctime>
+
+class ChapeuSeletor{
+public:
+  void recepcionar() const;
+  
+  std::string sortearCasa() const;
+};
