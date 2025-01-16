eb12fe57f3e271173895c2a5b6aa20188802e593
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2025-01-16 08:33:51 -0300

    Create Pessoa.h

---
+++ Atividade_5/Questao11/Pessoa.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Pessoa{
+  
+  friend std::ostream &operator<<(std::ostream &out, const Pessoa &pessoa){
+    out << "Nome: " << pessoa.nome << endl;
+    return out;
+  }
+  
+public:
+  Pessoa(std::string nome = "Sem nome"): nome{nome}{}
+  
+  void setNome(std::string nome){this->nome = nome;}
+  std::string getNome() const{return this->nome;}
+  
+protected:
+  std::string nome;
+};
