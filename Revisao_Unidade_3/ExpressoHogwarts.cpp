e68f3cfbea195f42ff81cb2b34c57e594f87eb57
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2025-01-30 15:37:14 -0300

    Add files via upload

---
+++ Revisao_Unidade_3/ExpressoHogwarts.cpp
@@ -0,0 +1,10 @@
+#include "ExpressoHogwarts.h"
+
+ExpressoHogwarts::ExpressoHogwarts &operator<<(const Bruxo &bruxo){
+    if(this->Passageiros.size() >= 100){
+        std::cout << "Limite de 100 passageiros atingido."
+        return *this;
+    }
+
+    this->Passageiros.push_back(new )    
+}
