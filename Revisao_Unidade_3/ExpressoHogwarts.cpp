5cdcce42f0cc8448b60ce5b5c5101d4af7665e2c
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2025-01-31 08:36:40 -0300

    Add files via upload

--- Revisao_Unidade_3/ExpressoHogwarts.cpp
+++ Revisao_Unidade_3/ExpressoHogwarts.cpp
@@ -1,10 +1,57 @@
 #include "ExpressoHogwarts.h"
 
-ExpressoHogwarts::ExpressoHogwarts &operator<<(const Bruxo &bruxo){
+std::ostream &operator<<(std::ostream &out, const ExpressoHogwarts &expresso){
+  if(expresso.Passageiros.size() <= 0){
+    out << "O expresso está vazio" << std::endl;
+    return out;
+  }
+  
+  out << "Passageiros no expresso:" << std::endl;
+  for(int i = 0; i < expresso.Passageiros.size(); i++)
+    out << "  " << expresso.Passageiros[i]->getNome() << std::endl;
+  
+  return out;
+}
+
+ExpressoHogwarts &ExpressoHogwarts::operator<<(const Bruxo &bruxo){
     if(this->Passageiros.size() >= 100){
-        std::cout << "Limite de 100 passageiros atingido."
+        std::cout << "Limite de 100 passageiros atingido.";
         return *this;
     }
 
-    this->Passageiros.push_back(new )    
+    this->Passageiros.push_back(new Bruxo(bruxo));
+    return *this;
 }
+
+ExpressoHogwarts &ExpressoHogwarts::operator<<(const Trouxa &trouxa){
+    if(this->Passageiros.size() >= 100){
+        std::cout << "Limite de 100 passageiros atingido.";
+        return *this;
+    }
+
+    this->Passageiros.push_back(new Trouxa(trouxa));
+    return *this;
+}
+
+ExpressoHogwarts &ExpressoHogwarts::operator>>(const std::string &nome){
+  for(int i = 0; i < this->Passageiros.size(); i++){
+    if(this->Passageiros[i]->getNome() == nome){
+      delete this->Passageiros[i];
+      this->Passageiros.erase(this->Passageiros.begin() + i);
+      return *this;
+    }
+  }
+  
+  return *this;
+}
+ 
+ExpressoHogwarts &ExpressoHogwarts::operator>>(const Pessoa &pessoa){
+  *this >> pessoa.getNome();
+  return *this;
+}
+
+ExpressoHogwarts::~ExpressoHogwarts(){
+  for(int i = 0; i < this->Passageiros.size(); i++)
+    delete this->Passageiros[i];
+}
+
